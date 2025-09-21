a48f5e3f31c795817e0bac16cf05847713cbb649
keertana <keerthu11vijay@gmail.com>
2025-09-22 01:26:39 +0800

    add missing files

--- pawsome/pawesome_diaries.js
+++
@@ -1,5 +0,0 @@
-function settingsDropdownToggle(button) {
-    var post = button.closest('.post');
-    var settingsDropdown = post.querySelector(".settings_dropdown");
-    settingsDropdown.classList.toggle("show_settings_dropdown");
-}
